f01b59560fe2a053e407873988f85f5424e60d62
yashpredator <yashrajsrivastava555@gmail.com>
2024-02-15 02:29:55 +0530

    Index.js updated for authentication. Still not tested

--- src/middleware/apierror.js
+++ src/middleware/apierror.js
@@ -1,10 +1,10 @@
-export const notFound = (req,res,next) => {
+const notFound = (req,res,next) => {
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
 }
 
-export const errorHandler = (err,req,res,next) =>{
+const errorHandler = (err,req,res,next) =>{
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
     if(err.name === 'CastError' && err.kind === 'ObjectId'){
@@ -17,3 +17,4 @@ export const errorHandler = (err,req,res,next) =>{
         stack:process.env.ENV === 'production' ? null:err.stack,
     });
 }   
+module.exports={notFound,errorHandler}
